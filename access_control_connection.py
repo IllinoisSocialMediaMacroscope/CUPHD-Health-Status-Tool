55ea36fbb560b9cb77b9e304db2eb27145fd37e5
Chen Wang <cwang138@illinois.edu>
2020-08-11 19:31:24 -0500

    add endpoint to set status

--- access_control_connection.py
+++ access_control_connection.py
@@ -17,6 +17,35 @@ def lookup_access_status(uin):
     else:
         return False, {"message": "Cannot connect to Access Control API!"}
 
+def update_access_status(uin, allowAccess: str):
+    """
+
+    :param uin:
+    :param allowAccess: "true" or "false"
+    :return:
+    """
+    headers = {
+        'x-api-key': config.ACCESSCTRL_KEY,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "uin": uin,
+        "allowAccess": allowAccess
+    }
+    result = requests.put(config.ACCESSCTRL_API_ENDPOINT, headers=headers, json=data)
+
+    if result.status_code == 200:
+        return True, {"data": result.json()}
+    elif result.status_code == 404:
+        return False, {"message": result.json()['message']}
+    else:
+        return False, {"message": "Cannot connect to Access Control API!"}
+
+
 if __name__ == "__main__":
     print(lookup_access_status(uin="123"))
-    print(lookup_access_status(uin="987654321"))
+    print(lookup_access_status(uin="668905810"))
+    print(update_access_status(uin="123", allowAccess="true"))
+    print(update_access_status(uin="668905810", allowAccess="true"))
