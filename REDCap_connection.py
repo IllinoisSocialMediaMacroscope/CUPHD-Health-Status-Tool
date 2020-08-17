c07ce7f35ce89cdbf38414ed7563a590ed20c808
Chen Wang <cwang138@illinois.edu>
2020-08-17 15:16:09 -0500

    change the test_id variable to be uin

--- REDCap_connection.py
+++ REDCap_connection.py
@@ -8,18 +8,18 @@ import config
 def set_REDCap_status(new_uin, new_status):
     # Fetch REDCap record for given new_uin and load it into dictionary
     data = {
-        'token': config.REDCAP_TOKEN,
-        'content': 'record',
-        'format': 'json',
-        'type': 'flat',
-        'csvDelimiter': '',
-        'rawOrLabel': 'raw',
-        'rawOrLabelHeaders': 'raw',
-        'exportCheckboxLabel': 'false',
-        'exportSurveyFields': 'false',
-        'exportDataAccessGroups': 'false',
-        'returnFormat': 'json',
-        'filterLogic': '[test_id] = ' + new_uin
+        "token": config.REDCAP_TOKEN,
+        "content": "record",
+        "format": "json",
+        "type": "flat",
+        "csvDelimiter": '',
+        "rawOrLabel": "raw",
+        "rawOrLabelHeaders": "raw",
+        "exportCheckboxLabel": "false",
+        "exportSurveyFields": "false",
+        "exportDataAccessGroups": "false",
+        "returnFormat": "json",
+        "filterLogic": "[uin] = " + new_uin
     }
     match_record_request = requests.post(config.REDCAP_API_ENDPOINT, data=data)
     if match_record_request.status_code == 200:
@@ -31,30 +31,30 @@ def set_REDCap_status(new_uin, new_status):
 
         # Update record with new_status and convert dictionary to string for import
         if new_status.lower() == "Quarantine".lower():
-            match_record['test_status'] = '1'
+            match_record["test_status"] = "1"
         elif new_status.lower() == "Isolate".lower():
-            match_record['test_status'] = '2'
+            match_record["test_status"] = "2"
         elif new_status.lower() == "Release.lower()":
-            match_record['test_status'] = '3'
+            match_record["test_status"] = "3"
         import_json = json.dumps(match_record)
 
         # overwrite record back into REDCap
         data = {
-            'token': config.REDCAP_TOKEN,
-            'content': 'record',
-            'format': 'json',
-            'type': 'flat',
-            'overwriteBehavior': 'normal',
-            'forceAutoNumber': 'false',
-            'data': "[" + import_json + "]",
-            'returnContent': 'count',
-            'returnFormat': 'json'
+            "token": config.REDCAP_TOKEN,
+            "content": "record",
+            "format": "json",
+            "type": "flat",
+            "overwriteBehavior": "normal",
+            "forceAutoNumber": "false",
+            "data": "[" + import_json + "]",
+            "returnContent": "count",
+            "returnFormat": "json"
         }
         overwrite_request = requests.post(config.REDCAP_API_ENDPOINT, data=data)
         if overwrite_request.status_code == 200:
             result = overwrite_request.json()
 
-            if result['count'] >= 1:
+            if result["count"] >= 1:
                 return True, {"data": "Successfully published new status to REDCAP"}
             else:
                 return False, {"message":"Cannot publish new status to REDCAP"}
